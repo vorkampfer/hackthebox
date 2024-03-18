36b3965a214c99b811c4cfb8d8801873f9616231
Ian Curtis <71790592+vorkampfer@users.noreply.github.com>
2024-03-18 22:31:31 +0000

    Add files via upload

---
+++ photobomb/cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+. /opt/.bashrc
+cd /home/wizard/photobomb
+
+# clean up log files
+if [ -s log/photobomb.log ] && ! [ -L log/photobomb.log ]
+then
+  /bin/cat log/photobomb.log > log/photobomb.log.old
+  /usr/bin/truncate -s0 log/photobomb.log
+fi
+
+# protect the priceless originals
+find source_images -type f -name '*.jpg' -exec chown root:root {} \;
+
